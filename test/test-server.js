f3128d2f436f758da93229a97d30cb779225138b
pranavkbansal <pranavkbansal@gmail.com>
2022-10-07 16:25:34 +0000

    commit 11:25 AM CST

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Native's application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Native's application from test branch</h1>"));
         return done();
       });
 
